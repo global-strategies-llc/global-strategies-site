2b89761ce171982039789eb1dbd99baa6f37d40c
Tor Gilbertson <tor@wisetail.com>
2019-10-15 23:28:33 -0600

    turn off insta

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -8,7 +8,7 @@ import './InstagramFeed.scss'
 
 export default class InstagramFeed extends Component {
   static defaultProps = {
-    accessToken: '1353697840.1677ed0.5a1cbfbc18f84915aa0d9a0bd02bff5a',
+    accessToken: '[API_ACCESS_TOKEN]',
     count: 20
   }
 
