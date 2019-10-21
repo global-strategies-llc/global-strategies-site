910e32ecb16ab568e12d12122827a9a47aa2237c
Tor Gilbertson <tor@wisetail.com>
2019-10-21 00:12:10 -0600

    rm contact form

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -3,7 +3,7 @@ import { MapPin, Smartphone, Mail } from 'react-feather'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
-import FormSimpleAjax from '../components/FormSimpleAjax'
+// import FormSimpleAjax from '../components/FormSimpleAjax'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
 import './ContactPage.scss'
@@ -54,13 +54,8 @@ export const ContactPageTemplate = ({
             )}
           </div>
         </div>
-
-        <div>
-          <FormSimpleAjax name="Simple Form Ajax" />
-        </div>
       </div>
     </section>
-
   </main>
 )
 
